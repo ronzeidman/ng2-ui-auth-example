bc824a5a28da2f07ff71dee79e2fd21faf41b65a
Royi Pressburger <royipr@amobee.com>
2016-10-09 20:52:54 +0300

    update to ionic rc0

---
+++ ionic2/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar } from 'ionic-native';
+import { Signup } from '../pages/signup/signup';
+
+
+@Component({
+  template: `<ion-nav [root]="rootPage"></ion-nav>`
+})
+export class MyApp {
+  rootPage = Signup;
+
+  constructor(platform: Platform) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+    });
+  }
+}
---
+++ ionic2/src/pages/login/login.ts
@@ -0,0 +1,55 @@
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import {NavController, LoadingController} from 'ionic-angular';
+import {TabsPage} from '../tabs/tabs';
+import {AuthenticationService} from '../../services/authentication/authentication-service';
+import {Signup} from '../signup/signup';
+
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html'
+})
+export class Login {
+  loginForm: FormGroup;
+  email: FormControl;
+  password: FormControl;
+  errorMessage;
+
+  ionViewDidLoad() {
+    this.email = this.fb.control('', Validators.compose([Validators.required]));
+    this.password = this.fb.control('', Validators.compose([Validators.required]));
+
+    this.loginForm = this.fb.group({
+      email: this.email,
+      password: this.password
+    });
+  }
+
+  login(value) {
+    this.logingIn(this.authService.login(value));
+  }
+
+  authenticate(provider: string) {
+    this.logingIn(this.authService.authenticate(provider));
+  }
+
+
+  signup() {
+    this.nav.push(Signup);
+  }
+
+  private logingIn(observable) {
+    let loading = this.loadingCtrl.create({
+      content: 'Please wait...',
+      dismissOnPageChange: true
+    });
+    loading.present();
+    observable.subscribe(() => this.nav.setRoot(TabsPage), err => console.log(err))
+  }
+
+  constructor(private nav: NavController,
+    private loadingCtrl: LoadingController,
+    private authService: AuthenticationService,
+    private fb: FormBuilder) { }
+
+}
